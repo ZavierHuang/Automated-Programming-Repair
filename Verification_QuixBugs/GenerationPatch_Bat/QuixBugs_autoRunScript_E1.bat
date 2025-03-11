3c95ed49214253b64fa93c8d349fe4e5345c3707
DESKTOP-3GT3IJ1\islab <t112598003@ntut.org.tw>
2025-03-11 17:25:48 +0800

    QuixBugs Epoch Shell Script

---
+++ Verification_QuixBugs/GenerationPatch_Bat/QuixBugs_autoRunScript_E1.bat
@@ -0,0 +1,79 @@
+#!/bin/bash
+@echo off
+echo "Run Start..."
+echo "================================================================"
+echo "==========                                            =========="
+echo "==========                                            =========="
+echo "========== Qwen Lora16 - Epoch 01 - Patch 01 =========="
+echo "==========                                            =========="
+echo "==========                                            =========="
+echo "================================================================"
+C:/Users/islab/anaconda3/envs/tensorflow_env/python.exe f:/My_APR/Experiment_CodeLlama/repairllama/src/lora/llama_pred_utf-8.py ^
+--do_sample True ^
+--only_do_beam True ^
+--only_do_topp False ^
+--only_do_topk False ^
+--only_do_temp False ^
+--temperature 0.8 ^
+--top_k 0 ^
+--top_p 0.95 ^
+--num_beams 1 ^
+--request_num 1 ^
+--data_path F:/My_APR/Experiment_CodeLlama/repairllama/Verification_QuixBugs ^
+--test_file QuixBugsProgram_Qwen_IR4OR2.jsonl ^
+--output_file F:/My_APR/Experiment_CodeLlama/repairllama/Verification_QuixBugs_Output/Result_Qwen_2.5/QuixBugs_Qwen_Lora16_E1_Patch01.jsonl ^
+--is_lora True ^
+--lora_path F:/My_APR/Experiment_CodeLlama/repairllama/model_Qwen/model_Lora16/checkpoint-epoch-1.0/ ^
+--base_model_path Qwen/Qwen2.5-Coder-1.5B
+
+echo "================================================================"
+echo "==========                                            =========="
+echo "==========                                            =========="
+echo "========== Qwen Lora16 - Epoch 01 - Patch 05 =========="
+echo "==========                                            =========="
+echo "==========                                            =========="
+echo "================================================================"
+C:/Users/islab/anaconda3/envs/tensorflow_env/python.exe f:/My_APR/Experiment_CodeLlama/repairllama/src/lora/llama_pred_utf-8.py ^
+--do_sample True ^
+--only_do_beam True ^
+--only_do_topp False ^
+--only_do_topk False ^
+--only_do_temp False ^
+--temperature 0.8 ^
+--top_k 0 ^
+--top_p 0.95 ^
+--num_beams 5 ^
+--request_num 5 ^
+--data_path F:/My_APR/Experiment_CodeLlama/repairllama/Verification_QuixBugs ^
+--test_file QuixBugsProgram_Qwen_IR4OR2.jsonl ^
+--output_file F:/My_APR/Experiment_CodeLlama/repairllama/Verification_QuixBugs_Output/Result_Qwen_2.5/QuixBugs_Qwen_Lora16_E1_Patch05.jsonl ^
+--is_lora True ^
+--lora_path F:/My_APR/Experiment_CodeLlama/repairllama/model_Qwen/model_Lora16/checkpoint-epoch-1.0/ ^
+--base_model_path Qwen/Qwen2.5-Coder-1.5B
+
+echo "================================================================"
+echo "==========                                            =========="
+echo "==========                                            =========="
+echo "========== Qwen Lora16 - Epoch 01 - Patch 10 =========="
+echo "==========                                            =========="
+echo "==========                                            =========="
+echo "================================================================"
+C:/Users/islab/anaconda3/envs/tensorflow_env/python.exe f:/My_APR/Experiment_CodeLlama/repairllama/src/lora/llama_pred_utf-8.py ^
+--do_sample True ^
+--only_do_beam True ^
+--only_do_topp False ^
+--only_do_topk False ^
+--only_do_temp False ^
+--temperature 0.8 ^
+--top_k 0 ^
+--top_p 0.95 ^
+--num_beams 10 ^
+--request_num 10 ^
+--data_path F:/My_APR/Experiment_CodeLlama/repairllama/Verification_QuixBugs ^
+--test_file QuixBugsProgram_Qwen_IR4OR2.jsonl ^
+--output_file F:/My_APR/Experiment_CodeLlama/repairllama/Verification_QuixBugs_Output/Result_Qwen_2.5/QuixBugs_Qwen_Lora16_E1_Patch10.jsonl ^
+--is_lora True ^
+--lora_path F:/My_APR/Experiment_CodeLlama/repairllama/model_Qwen/model_Lora16/checkpoint-epoch-1.0/ ^
+--base_model_path Qwen/Qwen2.5-Coder-1.5B
+
+echo "Run End"
