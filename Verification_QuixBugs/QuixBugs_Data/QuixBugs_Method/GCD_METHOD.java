ce8c8789b7e9868ccd2199925aa1268aba5f4108
DESKTOP-3GT3IJ1\islab <t112598003@ntut.org.tw>
2025-03-28 08:47:50 +0800

    Reset

--- Verification_QuixBugs/QuixBugs_Data/QuixBugs_Method/GCD_METHOD.java
+++
@@ -1,9 +0,0 @@
-public static int gcd(int a, int b) {
-    if (b == 0) {
-        return a;
-    } else {
-        // buggy code
-        // return gcd(a % b, b);
-        <FILL_ME>
-    }
-}
--- Verification_QuixBugs/QuixBugs_Data/QuixBugs_Method/SQRT_METHOD.java
+++
@@ -1,9 +0,0 @@
-public static double sqrt(double x, double epsilon) {
-    double approx = x / 2d;
-    // buggy code
-    // while (Math.abs(x-approx) > epsilon) {
-    <FILL_ME>
-        approx = 0.5d * (approx + x / approx);
-    }
-    return approx;
-}
